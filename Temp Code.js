089c309ae59b2177d30ae5b18a76082f03d1eca0
Amit Talapatra <amit.talapatra@gmail.com>
2020-05-01 16:35:49 -0400

    Modified Temp Code

--- Temp Code.js
+++ Temp Code.js
@@ -90,3 +90,35 @@ db.air_routes.aggregate([
         "stops" : 0,
         "airplane" : "CRJ"
 }
+
+db.air_airlines.aggregate([    { $limit: 1 }  ]).pretty()
+
+{
+        "_id" : ObjectId("56e9b497732b6122f8790280"),
+        "airline" : 4,
+        "name" : "2 Sqn No 1 Elementary Flying Training School",
+        "alias" : "",
+        "iata" : "WYT",
+        "icao" : "",
+        "active" : "N",
+        "country" : "United Kingdom",
+        "base" : "HGH"
+}
+
+var airlines = [];
+db.air_alliances.find({"name": "OneWorld"}).forEach(function(doc){
+  airlines = doc.airlines
+})
+var oneWorldAirlines = db.air_airlines.find({"name": {"$in": airlines}})
+
+oneWorldAirlines.forEach(function(airline){
+  db.air_alliances.aggregate([
+  {"$graphLookup": {
+    "startWith": airline.base,
+    "from": "air_routes",
+    "connectFromField": "dst_airport",
+    "connectToField": "src_airport",
+    "as": "connections",
+    "maxDepth": 1
+  }}])
+})
