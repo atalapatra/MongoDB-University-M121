7bb6090d7e68f3f7cec8f106e0e597e0eeec5c6f
Amit Talapatra <amit.talapatra@gmail.com>
2020-05-05 15:36:28 -0400

    Modified Temp Code

--- Temp Code.js
+++ Temp Code.js
@@ -42,68 +42,64 @@ db.movies.aggregate([
 ]).pretty()
 
 db.air_alliances.aggregate([    { $limit: 1 }  ]).pretty()
-{
-        "_id" : ObjectId("5980bef9a39d0ba3c650ae9b"),
-        "name" : "Star Alliance",
-        "airlines" : [
-                "Air Canada",
-                "Adria Airways",
-                "Avianca",
-                "Scandinavian Airlines",
-                "All Nippon Airways",
-                "Brussels Airlines",
-                "Shenzhen Airlines",
-                "Air China",
-                "Air New Zealand",
-                "Asiana Airlines",
-                "Brussels Airlines",
-                "Copa Airlines",
-                "Croatia Airlines",
-                "EgyptAir",
-                "TAP Portugal",
-                "United Airlines",
-                "Turkish Airlines",
-                "Swiss International Air Lines",
-                "Lufthansa",
-                "EVA Air",
-                "South African Airways",
-                "Singapore Airlines"
-        ]
-}
+// {
+//         "_id" : ObjectId("5980bef9a39d0ba3c650ae9b"),
+//         "name" : "Star Alliance",
+//         "airlines" : [
+//                 "Air Canada",
+//                 "Adria Airways",
+//                 "Avianca",
+//                 "Scandinavian Airlines",
+//                 "All Nippon Airways",
+//                 "Brussels Airlines",
+//                 "Shenzhen Airlines",
+//                 "Air China",
+//                 "Air New Zealand",
+//                 "Asiana Airlines",
+//                 "Brussels Airlines",
+//                 "Copa Airlines",
+//                 "Croatia Airlines",
+//                 "EgyptAir",
+//                 "TAP Portugal",
+//                 "United Airlines",
+//                 "Turkish Airlines",
+//                 "Swiss International Air Lines",
+//                 "Lufthansa",
+//                 "EVA Air",
+//                 "South African Airways",
+//                 "Singapore Airlines"
+//         ]
+// }
 
-db.air_routes.aggregate([
-...
-...   { $limit: 1 }
-...
-... ]).pretty()
-{
-        "_id" : ObjectId("56e9b39b732b6122f877fa96"),
-        "airline" : {
-                "id" : 470,
-                "name" : "Air Burkina",
-                "alias" : "2J",
-                "iata" : "VBW"
-        },
-        "src_airport" : "OUA",
-        "dst_airport" : "LFW",
-        "codeshare" : "",
-        "stops" : 0,
-        "airplane" : "CRJ"
-}
+db.air_routes.aggregate([{ $limit: 1 }]).pretty()
+// {
+//         "_id" : ObjectId("56e9b39b732b6122f877fa96"),
+//         "airline" : {
+//                 "id" : 470,
+//                 "name" : "Air Burkina",
+//                 "alias" : "2J",
+//                 "iata" : "VBW"
+//         },
+//         "src_airport" : "OUA",
+//         "dst_airport" : "LFW",
+//         "codeshare" : "",
+//         "stops" : 0,
+//         "airplane" : "CRJ"
+// }
 
 db.air_airlines.aggregate([    { $limit: 1 }  ]).pretty()
 
-{
-        "_id" : ObjectId("56e9b497732b6122f8790280"),
-        "airline" : 4,
-        "name" : "2 Sqn No 1 Elementary Flying Training School",
-        "alias" : "",
-        "iata" : "WYT",
-        "icao" : "",
-        "active" : "N",
-        "country" : "United Kingdom",
-        "base" : "HGH"
-}
+// {
+//         "_id" : ObjectId("56e9b497732b6122f8790280"),
+//         "airline" : 4,
+//         "name" : "2 Sqn No 1 Elementary Flying Training School",
+//         "alias" : "",
+//         "iata" : "WYT",
+//         "icao" : "",
+//         "active" : "N",
+//         "country" : "United Kingdom",
+//         "base" : "HGH"
+// }
 
 var airlines = [];
 db.air_alliances.find({"name": "OneWorld"}).forEach(function(doc){
